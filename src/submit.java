8787f67e08396f5a4dc738d800dbdcf14822b4e4
maicongs_ <maicongs.sta@gmail.com>
2021-11-11 21:38:05 -0300

    Update submit.java

--- src/submit.java
+++ src/submit.java
@@ -6,6 +6,44 @@ public class submit {
 	private String titulo;
 	private String palavra_chave;
 	private String resumo;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	public String getOrientador() {
+		return orientador;
+	}
+	public void setOrientador(String orientador) {
+		this.orientador = orientador;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getPalavra_chave() {
+		return palavra_chave;
+	}
+	public void setPalavra_chave(String palavra_chave) {
+		this.palavra_chave = palavra_chave;
+	}
+	public String getResumo() {
+		return resumo;
+	}
+	public void setResumo(String resumo) {
+		this.resumo = resumo;
+	}
+	
+	
 	
 	
 }
