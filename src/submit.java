e94d720a6268d67dc62eeb996d132e69e6fc6ce9
maicongs_ <maicongs.sta@gmail.com>
2021-11-11 21:37:10 -0300

    Inicio do Projeto

---
+++ src/submit.java
@@ -0,0 +1,11 @@
+
+public class submit {
+	private String nome;
+	private String curso;
+	private String orientador;
+	private String titulo;
+	private String palavra_chave;
+	private String resumo;
+	
+	
+}
